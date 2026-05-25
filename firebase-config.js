1842a7e2e80f5d08b7a9ef59ebb47c762fad3cac
NaJaeHeum <gmd1356@gmail.com>
2026-05-26 01:14:21 +0900

    Enable Firebase shared editing

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-// Copy your Firebase web app config here to enable shared cloud plans.
-// Firebase web config is not a server secret, but restrict your database rules before sharing widely.
-window.FIREBASE_CONFIG = null;
+// Firebase web config is not a server secret, but restrict database rules before sharing widely.
+window.FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyA96-LLEuuAZjj-WV8xnRLkbFKzIWsjJf0',
+    authDomain: 'ffxiv-mitigation-planner.firebaseapp.com',
+    databaseURL: 'https://ffxiv-mitigation-planner-default-rtdb.firebaseio.com',
+    projectId: 'ffxiv-mitigation-planner',
+    storageBucket: 'ffxiv-mitigation-planner.firebasestorage.app',
+    messagingSenderId: '284602414108',
+    appId: '1:284602414108:web:76162d7977dc6ac8171bd9',
+    measurementId: 'G-3JPQLL33MQ',
+};
 window.FIREBASE_PLAN_ID = 'default';
